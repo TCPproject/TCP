4feac5da8d158f95f4219d71720a4ccda6639a3c
Orange13378 <51047366+Orange13378@users.noreply.github.com>
2020-10-22 01:27:56 +0300

    Unity Project

---
+++ RAGU/Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
