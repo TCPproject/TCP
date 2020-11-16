1d4fc97a3cf8ec75860a88dd33848381b4c59a39
mishasty228 <50260567+mishasty228@users.noreply.github.com>
2020-11-16 21:20:10 +0300

    webupd

---
+++ web/TCPproject/TCPproject/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace TCPproject.Controllers
+{
+    public class BaseController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
