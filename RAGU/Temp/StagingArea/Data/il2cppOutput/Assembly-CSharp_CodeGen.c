ac1f22999514c2808e0425ef5981531a2a227524
davidbrum <brumdd@mail.ru>
2020-12-07 12:12:45 +0300

    RAGU_update

---
+++ RAGU/Temp/StagingArea/Data/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,83 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void Enemy_AI::Start()
+extern void Enemy_AI_Start_mC651FD3C74FFF285FD7CD2E4AC367C9D49B998D3 (void);
+// 0x00000002 System.Void Enemy_AI::Update()
+extern void Enemy_AI_Update_m20F0C16738982052FDA5CB80FF3191C561A5A8BF (void);
+// 0x00000003 System.Void Enemy_AI::.ctor()
+extern void Enemy_AI__ctor_m53EC23CF72985E013A6347A45B45ACE90C6BAF35 (void);
+// 0x00000004 System.Void FollowMouse::Update()
+extern void FollowMouse_Update_m90F0D4CE64BD9C6FB948A7506736B5817E8B35B6 (void);
+// 0x00000005 System.Void FollowMouse::.ctor()
+extern void FollowMouse__ctor_mDC95389AFDA729B0C109227F365600F508C74B49 (void);
+// 0x00000006 System.Void HeroShopManager::Start()
+extern void HeroShopManager_Start_m127B9A3275EE0919A6FB132FE68AFA08D7F920C3 (void);
+// 0x00000007 System.Void HeroShopManager::Update()
+extern void HeroShopManager_Update_m61B516B30B71662D35639B0D56A76CF00689EE45 (void);
+// 0x00000008 System.Void HeroShopManager::.ctor()
+extern void HeroShopManager__ctor_m1F5DF3EA977D09950382945440142F827FA05725 (void);
+// 0x00000009 System.Void SpawnHeroes::Update()
+extern void SpawnHeroes_Update_m9EC560548C09F55AA3190D0C3171FE73BC024BF8 (void);
+// 0x0000000A System.Void SpawnHeroes::.ctor()
+extern void SpawnHeroes__ctor_mD74ACABD51FC1E103CBBA974A4DDF15B2C6EDF73 (void);
+static Il2CppMethodPointer s_methodPointers[10] = 
+{
+	Enemy_AI_Start_mC651FD3C74FFF285FD7CD2E4AC367C9D49B998D3,
+	Enemy_AI_Update_m20F0C16738982052FDA5CB80FF3191C561A5A8BF,
+	Enemy_AI__ctor_m53EC23CF72985E013A6347A45B45ACE90C6BAF35,
+	FollowMouse_Update_m90F0D4CE64BD9C6FB948A7506736B5817E8B35B6,
+	FollowMouse__ctor_mDC95389AFDA729B0C109227F365600F508C74B49,
+	HeroShopManager_Start_m127B9A3275EE0919A6FB132FE68AFA08D7F920C3,
+	HeroShopManager_Update_m61B516B30B71662D35639B0D56A76CF00689EE45,
+	HeroShopManager__ctor_m1F5DF3EA977D09950382945440142F827FA05725,
+	SpawnHeroes_Update_m9EC560548C09F55AA3190D0C3171FE73BC024BF8,
+	SpawnHeroes__ctor_mD74ACABD51FC1E103CBBA974A4DDF15B2C6EDF73,
+};
+static const int32_t s_InvokerIndices[10] = 
+{
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	10,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
