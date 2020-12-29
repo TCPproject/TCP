a0978bcc305c4ea47a19da1454b3ac1bd80f2681
mishasty228 <mishasty2001@gmail.com>
2020-12-29 20:11:20 +0300

    wowsuchsaltyhashedpasswords

--- web/TCP/TCP/ViewModels/LoginModel.cs
+++ web/TCP/TCP/ViewModels/LoginModel.cs
@@ -3,16 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
 
 namespace TCP.ViewModels
 {
     public class LoginModel
     {
+        private string email;
+        private string password;
+
+
         [Required(ErrorMessage = "Не указан Email")]
-        public string Email { get; set; }
+        public string Email 
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
 
         [Required(ErrorMessage = "Не указан пароль")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password 
+        { 
+            get
+            {
+                return password;
+            }
+            set
+            {
+                byte[] salt = Encoding.ASCII.GetBytes(email);
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(salt);
+                }
+                password = value;
+            }
+        }
     }
 }
