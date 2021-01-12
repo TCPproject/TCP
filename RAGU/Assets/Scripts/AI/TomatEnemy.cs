21ff256347d91b23e652e1d04da119d01a566a28
Orange13378 <51047366+Orange13378@users.noreply.github.com>
2021-01-12 15:13:19 +0300

    AI alfa

---
+++ RAGU/Assets/Scripts/AI/TomatEnemy.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TomatEnemy : MonoBehaviour
+{
+    public float health = 100;
+    public float Damage = 5;
+    private bool attack;
+    private float stopingdistance = 2f;
+    private float agro = 10f;
+    protected Transform basa;
+    private NavMeshAgent agent;
+    private Transform ogurec;
+    private bool stop;
+
+    void Start()
+    {
+        ogurec = GameObject.FindGameObjectWithTag("Ogurec").GetComponent<Transform>();
+        basa = GameObject.FindGameObjectWithTag("Finish").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = 4;
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.avoidancePriority = Random.Range(1, 100);
+    }
+
+    void Update()
+    {   
+        //Счетчик всего ботов и если он 0 то проигрыш
+                if (ogurec != null)
+                {
+                    //agent.Resume();
+                    agent.SetDestination(ogurec.position);
+                }
+                if (ogurec == null)
+                {
+                    agent.Stop();
+                    ogurec = GameObject.FindGameObjectWithTag("Ogurec").GetComponent<Transform>();
+                    //Debug.Log("ID" + ogurec.GetInstanceID());
+                    agent.SetDestination(ogurec.position);
+                    agent.Resume();
+                }
+            
+
+
+
+
+
+        //if ((transform.position - target.transform.position).magnitude < 1 && !stop)
+        //{
+        //    stop = true;
+        //    target.GetComponent<Navigate>().enabled = false;
+
+        //    //var win = FindObjectOfType<Continue>();
+        //    //win.WinCondition();
+        //}
+
+        //Navigate.DebugDrawPath(agent.path.corners);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        OgurEnemy ogurEnemy = other.gameObject.GetComponent<OgurEnemy>();
+
+        if (ogurEnemy != null)
+        {
+            ogurEnemy.TakeDamage(Damage);
+            //anima.Play("Bo");
+            StartCoroutine(Dead());
+        }
+    }
+
+    public void TakeDamage(float Damage)
+    {
+        health -= Damage;
+        Debug.Log("Ogurec HP: " + health);
+        if (health <= 0)
+        {
+            StartCoroutine(Dead());
+        }
+    }
+
+    IEnumerator Dead()
+    {
+        float number1 = Random.Range(0.15f, 0.2f);
+        yield return new WaitForSeconds(number1);
+        //dead++;
+        //PlayerPrefs.SetInt("Dead", dead);
+        Destroy(gameObject);
+    }
+}
--- RAGU/Assets/Scripts/Base.cs
+++ RAGU/Assets/Scripts/Base.cs
@@ -5,45 +5,21 @@ using UnityEngine.UI;
 
 public class Base : MonoBehaviour
 {
-    [SerializeField] protected GameObject DeadMenu, PauseMenu, Joy1, Joy2, But, nextwave, compl, startwave;
+    //[SerializeField] protected GameObject DeadMenu, PauseMenu, Joy1, Joy2, But, nextwave, compl, startwave;
     public float health = 100;
     private float healthUI;
-    public GameObject base0, base1, base2, base3, base4, base5;
+    //public GameObject base0, base1, base2, base3, base4, base5;
     public Image UIHPBase;
     void Start()
     {
-        base0.SetActive(true);
         healthUI = health;
     }
+
     void Update()
     {
         UIHPBase.fillAmount = health / healthUI;
-        if (health <= 90 && health > 70)
-        {
-            base0.SetActive(false);
-            base1.SetActive(true);
-        }
-        if (health <= 70 && health > 50)
-        {
-            base1.SetActive(false);
-            base2.SetActive(true);
-        }
-        if (health <= 50 && health > 25)
-        {
-            base2.SetActive(false);
-            base3.SetActive(true);
-        }
-        if (health <= 25 && health > 10)
-        {
-            base3.SetActive(false);
-            base4.SetActive(true);
-        }
-        if (health <= 10)
-        {
-            base4.SetActive(false);
-            base5.SetActive(true);
-        }
     }
+
     public void TakeDamage(float Damage)
     {
         health -= Damage;
@@ -52,22 +28,22 @@ public class Base : MonoBehaviour
             StartCoroutine(Dead());
         }
     }
-    public void Deady(GameObject a, GameObject b, GameObject c, GameObject d, GameObject e, GameObject f, GameObject g, GameObject k)
-    {
-        a.SetActive(false);
-        b.SetActive(false);
-        c.SetActive(false);
-        d.SetActive(true);
-        e.SetActive(false);
-        f.SetActive(false);
-        g.SetActive(false);
-        k.SetActive(false);
-        Time.timeScale = 0;
-    }
+
     IEnumerator Dead()
     {
         yield return new WaitForSeconds(0.18f);
-        Deady(Joy1, Joy2, PauseMenu, DeadMenu, But, nextwave, compl, startwave);
-        DestroyImmediate(gameObject);
+        Destroy(gameObject);
     }
+    //public void Deady(GameObject a, GameObject b, GameObject c, GameObject d, GameObject e, GameObject f, GameObject g, GameObject k)
+    //{
+    //    a.SetActive(false);
+    //    b.SetActive(false);
+    //    c.SetActive(false);
+    //    d.SetActive(true);
+    //    e.SetActive(false);
+    //    f.SetActive(false);
+    //    g.SetActive(false);
+    //    k.SetActive(false);
+    //    Time.timeScale = 0;
+    //}
 }
